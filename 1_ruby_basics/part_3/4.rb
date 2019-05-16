80c3bd39ab154ef7b44eaab09cba820bb0c579c4
alkazarini <alex.kazarov@gmail.com>
2019-05-16 20:51:18 +0300

    Errors fix

--- 1_ruby_basics/part_3/4.rb
+++ 1_ruby_basics/part_3/4.rb
@@ -1,1 +1,5 @@
-puts ('а'..'я').to_a.map!.with_index { |letter, idx| [letter.to_sym, idx + 1] if ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'].include?(letter) }.compact!.to_h
+puts (('а'..'я').to_a.map!.with_index do |letter, idx| 
+[letter.to_sym, idx + 1] if ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'].include?(letter)
+end).to_a.compact!.to_h
+
+
