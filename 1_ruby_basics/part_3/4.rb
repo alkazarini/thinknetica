f1e640913192947944e2769749316060e0d46424
alkazarini <alex.kazarov@gmail.com>
2019-05-16 20:56:21 +0300

    Errors fix

--- 1_ruby_basics/part_3/4.rb
+++ 1_ruby_basics/part_3/4.rb
@@ -1,3 +1,3 @@
 puts (('а'..'я').to_a.map!.with_index do |letter, idx| 
 [letter.to_sym, idx + 1] if ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'].include?(letter)
-end).to_a.compact!.to_h
+end).compact!.to_h
