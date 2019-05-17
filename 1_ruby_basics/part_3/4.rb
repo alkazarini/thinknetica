9cd2a3682110d0e930406a9e5635f5bc557f956f
alkazarini <alex.kazarov@gmail.com>
2019-05-17 17:51:31 +0300

    Errors fix

--- 1_ruby_basics/part_3/4.rb
+++ 1_ruby_basics/part_3/4.rb
@@ -1,3 +1,8 @@
-puts (('а'..'я').to_a.map!.with_index do |letter, idx| 
-[letter.to_sym, idx + 1] if ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'].include?(letter)
-end).compact!.to_h
+alphabet = ('а'..'я')
+vowels = {}
+
+alphabet.each_with_index do |letter, index| 
+  vowels[letter] =  index + 1 if ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'].include?(letter)
+end
+
+puts vowels
