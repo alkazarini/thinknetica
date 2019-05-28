0eed91b81c494c9d99aade40765eec63ccae6d1e
alkazarini <alex.kazarov@gmail.com>
2019-05-28 18:38:15 +0300

    Fix errors

--- 1_ruby_basics/part_4/train.rb
+++ 1_ruby_basics/part_4/train.rb
@@ -34,18 +34,18 @@ class  Train
   end
 
   def move_forward
-    @station = next_station
+    @station = next_station if next_station
   end
 
   def move_backward
-    @station = previous_station if previous_station != nil 
+    @station = previous_station if previous_station 
   end
 
   def next_station
-    (@route.index(@station) + 1) < @route.size ? (@route[@route.index(@station) + 1]) : (@station)
+    @route[@route.index(@station) + 1] if @route.index(@station) -1 < @route.size
   end
 
   def previous_station
-    (@route.index(@station) - 1) >=0 ? (@route[@route.index(@station) - 1]) : nil
+    @route[@route.index(@station) - 1] if @route.index(@station) - 1 >= 0 
   end
 end
